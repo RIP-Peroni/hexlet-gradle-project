8097f5788c279b387c24970e16e0bda3e5717801
Yuriy Epikhin <epih96@mail.ru>
2023-02-17 19:28:17 +0300

    TreeMap

--- app/src/main/java/hexlet/gradle/project/App.java
+++ app/src/main/java/hexlet/gradle/project/App.java
@@ -1,22 +1,22 @@
 package main.java.hexlet.gradle.project;
 
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class App {
     public static void main(String[] args) {
-        Map<Integer, String> lhm = new CustomLinkedHM();
-        lhm.put(1, "");
-        lhm.put(2, "");
-        lhm.put(3, "");
-        System.out.println(lhm);
-        lhm.get(1);
+        Map<Integer, String> hm = new HashMap<>();
+        hm.put(1231, "Linus Torvalds");
+        hm.put(36533, "Donald Knuth");
+        hm.put(293216, "Larry Page");
+        hm.put(246345, "Anders Hejlberg");
+        System.out.println(hm);
 
-        System.out.println(lhm);
-        lhm.put(4, "");
-        System.out.println(lhm);
-        lhm.put(5, "");
-        System.out.println(lhm);
+        Map<Product, String> tm = new TreeMap<>();
+        tm.put(new Product("milk", 70, 5), "drinks");
+        tm.put(new Product("chocolate", 105, 3), "food");
+        tm.put(new Product("eggs", 8, 3), "food");
+        System.out.println(tm);
     }
 }
