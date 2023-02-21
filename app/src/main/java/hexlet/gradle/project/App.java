18a08867d42af07b25af986000cf3ede4bf70dc9
Yuriy Epikhin <y.epikhin@intickets.ru>
2023-02-21 08:01:08 +0300

    gen diff homework

--- app/src/main/java/hexlet/gradle/project/App.java
+++ app/src/main/java/hexlet/gradle/project/App.java
@@ -1,15 +1,35 @@
 package main.java.hexlet.gradle.project;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class App {
+    public static Map<String, String> genDiff(Map<String, Object> map1, Map<String, Object> map2) {
+        Map<String, String> result = new LinkedHashMap<>();
+        Set<String> uniqueKeys = new TreeSet<>();
+        uniqueKeys.addAll(map1.keySet());
+        uniqueKeys.addAll(map2.keySet());
+        for (String key: uniqueKeys) {
+            Object oldValue = map1.get(key);
+            Object newValue = map2.get(key);
+            if (!map2.containsKey(key)) {
+                result.put(key, "deleted");
+            } else if (!map1.containsKey(key)) {
+                result.put(key, "added");
+            } else if (!Objects.equals(oldValue, newValue)) {
+                result.put(key, "changed");
+            } else {
+                result.put(key, "unchanged");
+            }
+        }
+        return result;
+    }
     public static void main(String[] args) {
-        Map<Product, String> tm = new TreeMap<>(new ProductQualityComparator());
-        tm.put(new Product("milk", 200, 5), "drinks");
-        tm.put(new Product("chocolate", 105, 10), "food");
-        tm.put(new Product("eggs", 8, 3), "food");
-        System.out.println(tm);
+        Map<String, Object> data1 = new HashMap<>(
+                Map.of("one", "eon", "two", "two", "four", true, "abs", 'h')
+        );
+        Map<String, Object> data2 = new HashMap<>(
+                Map.of("two", "own", "zero", 4, "four", true)
+        );
+        System.out.println(genDiff(data1, data2));
     }
 }
